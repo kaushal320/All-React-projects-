064b87c7194029f1595fbb4e7126e8635abb6f60
kaushal <127113014+kaushal320@users.noreply.github.com>
2025-01-31 11:34:59 +0545

    Create App.jsx

---
+++ First_React_Project/react-first/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ProductInfo from "./components/ProductInfo";
+
+const App = () => {
+  return (
+    <>
+      <ProductInfo />
+    </>
+  );
+};
+
+export default App;
