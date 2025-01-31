7b05e8365c1b0157b4e071f4a7fd5ce3d6d1bc0d
kaushal <127113014+kaushal320@users.noreply.github.com>
2025-01-31 11:35:21 +0545

    Create ProductInfo.jsx

---
+++ First_React_Project/react-first/src/components/ProductInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+const ProductInfo = () => {
+  const [movie, setMovie] = useState({
+    name: "Hello",
+    price: 200,
+    rating: 7,
+  });
+  return <></>;
+};
+
+export default ProductInfo;
