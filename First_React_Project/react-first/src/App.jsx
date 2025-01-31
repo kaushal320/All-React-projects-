a658ff2fd81ce31fe5234eb79f7154710e1df8dd
kaushal <127113014+kaushal320@users.noreply.github.com>
2025-01-31 18:37:00 +0545

    Update App.jsx

--- First_React_Project/react-first/src/App.jsx
+++ First_React_Project/react-first/src/App.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { createContext, useReducer } from "react";
 
 import ProductInfo from "./components/ProductInfo";
-
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+      break;
+    default:
+      break;
+  }
+};
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <ProductInfo />
